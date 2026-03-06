4611f44007bb57dcd1f06edd1aa4790b9b701964
Bruno Haible <bruno@clisp.org>
2026-03-06 15:05:38 +0100

    Prepare for version 1.19.
    * configure.ac: Bump version number to 1.19.
    * include/iconv.h.in (_LIBICONV_VERSION): Likewise.
    * lib/Makefile.in (LIBICONV_VERSION_INFO): Bump to 9:1:7.
    * src/iconv.c (print_version): Update copyright year.
    * windows/iconv.rc: Likewise.
    * windows/libiconv.rc: Likewise.
    * NEWS: Update.
    * README: Update download link.

--- src/iconv.c
+++ src/iconv.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000-2024 Free Software Foundation, Inc.
+/* Copyright (C) 2000-2026 Free Software Foundation, Inc.
    This file is part of the GNU LIBICONV Library.
 
    This program is free software: you can redistribute it and/or modify
@@ -200,7 +200,7 @@ static void print_version (void)
 {
   printf("iconv (GNU libiconv %d.%d)\n",
          _libiconv_version >> 8, _libiconv_version & 0xff);
-  printf("Copyright (C) %s Free Software Foundation, Inc.\n", "2000-2024");
+  printf("Copyright (C) %s Free Software Foundation, Inc.\n", "2000-2026");
   /* xgettext: no-wrap */
   /* TRANSLATORS: The %s placeholder is the web address of the GPL license.  */
   printf (_("\
